ed04fe8a3c68d765e6194f6a5f38776907067108
santiago.garcia@sdggroup.com <santiago.garcia@sdggroup.com>
2021-04-27 15:16:35 +0000

    New model: tmp_user_territory

---
+++ models/tmp_user_territory.sql
@@ -0,0 +1,17 @@
+{{ config(
+	post_hook="grant all privileges on {{ this }} to {{ var('snowflake_user_grant_privileges') }}")
+}}
+
+SELECT ut_uniq.id, ut_uniq.userid, ut.territory2id as territoryid
+FROM 
+(
+	SELECT max(id) as "id", userid
+	FROM 
+	(
+		SELECT ut.Id, ut.UserId
+		FROM {{ var('schema') }}.user_territory_association_raw ut
+		JOIN {{ var('schema') }}.user_raw u on u.id = ut.userid AND u.isactive != 0
+	)
+	GROUP BY userid
+) ut_uniq 
+JOIN {{ var('schema') }}.user_territory_association_raw ut on ut.id = ut_uniq.id

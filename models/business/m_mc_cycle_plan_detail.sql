fbcf04597485639e5a5dbc649d345d622feb89d1
sgarci34@its.jnj.com <sgarci34@its.jnj.com>
2021-05-14 08:46:38 +0000

    Fix issue: m_mc_cycle_plan_detail

--- models/business/m_mc_cycle_plan_detail.sql
+++ models/business/m_mc_cycle_plan_detail.sql
@@ -5,38 +5,38 @@
 with cte_aux_m_mc_cycle_plan_detail as
 (
   select distinct
-         isnull(cplan.Id,'') as mc_cycle_plan_id,
-         isnull(cptarget.Id,'') as mc_cycle_plan_target_id,
-         isnull(cptarget.target_vod__c,'') as account_id,
-         isnull(cpchannel.id,'') as mc_cycle_plan_channel_id,
-         isnull(cpproduct.Cycle_Product_vod__c,'') as mc_cycle_product_id,
-         isnull(cpproduct.Id,'') as mc_cycle_plan_product_id,
+         ifnull(cplan.Id,'') as mc_cycle_plan_id,
+         ifnull(cptarget.Id,'') as mc_cycle_plan_target_id,
+         ifnull(cptarget.target_vod__c,'') as account_id,
+         ifnull(cpchannel.id,'') as mc_cycle_plan_channel_id,
+         ifnull(cpproduct.Cycle_Product_vod__c,'') as mc_cycle_product_id,
+         ifnull(cpproduct.Id,'') as mc_cycle_plan_product_id,
          
-         cast(case when isnull(cpproduct.Product_Activity_Goal_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Goal_vod__c end as float8) as mc_cycle_plan_product_activity_goal,
-         cast(case when isnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8) as mc_cycle_plan_product_activity_actual,
-         cast(case when isnull(cpproduct.Product_Activity_Remaining_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Remaining_vod__c end as float8) as mc_cycle_plan_product_activity_remaining,
+         cast(case when ifnull(cpproduct.Product_Activity_Goal_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Goal_vod__c end as float8) as mc_cycle_plan_product_activity_goal,
+         cast(case when ifnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8) as mc_cycle_plan_product_activity_actual,
+         cast(case when ifnull(cpproduct.Product_Activity_Remaining_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Remaining_vod__c end as float8) as mc_cycle_plan_product_activity_remaining,
 
          case
-           when (cast(case when isnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
-              <= (cast(case when isnull(cpproduct.Product_Activity_Goal_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Goal_vod__c end as float8))
-             then (cast(case when isnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
-           else (cast(case when isnull(cpproduct.Product_Activity_Goal_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Goal_vod__c end as float8))
+           when (cast(case when ifnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
+              <= (cast(case when ifnull(cpproduct.Product_Activity_Goal_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Goal_vod__c end as float8))
+             then (cast(case when ifnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
+           else (cast(case when ifnull(cpproduct.Product_Activity_Goal_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Goal_vod__c end as float8))
          end as mc_cycle_plan_product_activity_valid,    
          
 
-         isnull(cpproduct.Product_Name_vod__c, '') as mc_cycle_plan_product,
+         ifnull(cpproduct.Product_Name_vod__c, '') as mc_cycle_plan_product,
          
-         cast(case when isnull(cpproduct.Product_Activity_Max_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Max_vod__c end as float8) as mc_cycle_plan_product_activity_max,
+         cast(case when ifnull(cpproduct.Product_Activity_Max_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Max_vod__c end as float8) as mc_cycle_plan_product_activity_max,
          
          case
-            when (cast(case when isnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
-               >  (cast(case when isnull(cpproduct.Product_Activity_Max_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Max_vod__c end as float8))
-              then  (cast(case when isnull(cpproduct.Product_Activity_Max_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Max_vod__c end as float8))
-            else (cast(case when isnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
+            when (cast(case when ifnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
+               >  (cast(case when ifnull(cpproduct.Product_Activity_Max_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Max_vod__c end as float8))
+              then  (cast(case when ifnull(cpproduct.Product_Activity_Max_vod__c,'') = '' then '0' else cpproduct.Product_Activity_Max_vod__c end as float8))
+            else (cast(case when ifnull(cpproduct.Product_Actual_Activity_vod__c,'') = '' then '0' else cpproduct.Product_Actual_Activity_vod__c end as float8))
          end as mc_cycle_plan_product_activity_actual_compared_to_max,
          
          
-         isnull(cproduct.Product_vod__c, '') as product_id
+         ifnull(cproduct.Product_vod__c, '') as product_id
     from      {{ source('raw', 'mc_cycle_plan') }}         as cplan
     left join {{ source('raw', 'mc_cycle_plan_target') }}  as cptarget
       on cptarget.cycle_plan_vod__c 											                = cplan.id
@@ -52,10 +52,10 @@ select mc_cycle_plan_channel_id || '-' || account_id::varchar(50) as mc_channel_
        mc_cycle_plan_product_activity_goal::smallint as mc_cycle_plan_product_activity_goal,
        mc_cycle_plan_product_activity_actual::smallint as mc_cycle_plan_product_activity_actual,
        mc_cycle_plan_product_activity_remaining::smallint as mc_cycle_plan_product_activity_remaining,
-       mc_cycle_plan_product_activity_valid::varchar(50) as mc_cycle_plan_product_activity_valid,
-       mc_cycle_plan_product) as mc_cycle_plan_product,
+       mc_cycle_plan_product_activity_valid::smallint as mc_cycle_plan_product_activity_valid,
+       mc_cycle_plan_product::varchar(50) as mc_cycle_plan_product,
        mc_cycle_plan_product_activity_max::smallint as mc_cycle_plan_product_activity_max,
        mc_cycle_plan_product_activity_actual_compared_to_max::smallint as mc_cycle_plan_product_activity_actual_compared_to_max,
        case when mc_cycle_plan_product_activity_goal = '0' then '0' else (mc_cycle_plan_product_activity_actual_compared_to_max / mc_cycle_plan_product_activity_goal) end::numeric(18,0) as mc_cycle_plan_product_attainment,
        case when mc_cycle_plan_product_activity_goal = '0' then '0' else (mc_cycle_plan_product_activity_valid / mc_cycle_plan_product_activity_goal) end::numeric(18,0) as mc_cycle_plan_product_completion      
-  from cte_aux_m_mc_cycle_plan_detail;
+  from cte_aux_m_mc_cycle_plan_detail

40396a7c08675618a877d95a08ceeaaf13173968
sgarci34@its.jnj.com <sgarci34@its.jnj.com>
2021-05-14 10:42:51 +0000

    Fix issues: m_profile

--- models/business/m_profile.sql
+++ models/business/m_profile.sql
@@ -6,19 +6,19 @@ with AUXX_TABLE as (
 
 SELECT 
 AUX_TABLE.*,
-JJC.jj_region__c::varchar(255) AS "Profile_Region", 
-JJC.name::varchar(255) as "Country",
-PR.Name::varchar(255) as "Profile"
+JJC.jj_region__c::varchar(255) AS Profile_Region, 
+JJC.name::varchar(255) as Country,
+PR.Name::varchar(255) as Profile
 
 FROM (
 	SELECT
- 	u1.profileid::varchar(255) as "profile_id", 
-	u2.country_iso_code::varchar(255) as "profile_country_code"
+ 	u1.profileid::varchar(255) as profile_id, 
+	u2.country_iso_code::varchar(255) as profile_country_code
 	FROM (
 		SELECT 
 			ROW_NUMBER() OVER (PARTITION BY profileid ORDER BY lastmodifieddate DESC) as rowid, 
-			USR.id as "userid", 
-			USR.profileid as "profileid"
+			USR.id as userid, 
+			USR.profileid as profileid
 		FROM {{ source('raw', 'user') }} as USR 
 		) AS u1	
 	LEFT OUTER JOIN {{ source('raw', 'user') }} as u2 ON u1.userid = u2.id
@@ -32,11 +32,10 @@ ORDER BY profile_id )
 
 
 SELECT DISTINCT 
-	id::varchar(255) as "profile_id", 
-	name::varchar(255) as "profile", 
-	CASE WHEN profile_country_code = '' THEN 'NM' ELSE COALESCE(profile_country_code, 'NM') END::varchar(255) as "profile_country_code", 
-	COALESCE(Country, 'NM')::varchar(255) as "profile_country",
-	COALESCE(profile_region, 'NM')::varchar(255) as "Profile_region" 
-FROM {{ source('raw', 'medical_insight') }}.zz_fwk_icon_profile_business as TMP
+	id::varchar(255) as profile_id, 
+	name::varchar(255) as profile, 
+	CASE WHEN profile_country_code = '' THEN 'NM' ELSE COALESCE(profile_country_code, 'NM') END::varchar(255) as profile_country_code, 
+	COALESCE(Country, 'NM')::varchar(255) as profile_country,
+	COALESCE(profile_region, 'NM')::varchar(255) as Profile_region 
+FROM {{ source('raw', 'profile') }} as TMP
 LEFT OUTER JOIN AUXX_TABLE ON AUXX_TABLE.profile_id = tmp.id
-;

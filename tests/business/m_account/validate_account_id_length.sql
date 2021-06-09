77cf474cadd51121700bfd3430c6fa54085347a4
sgarci34@its.jnj.com <sgarci34@its.jnj.com>
2021-06-09 11:30:09 +0000

    documentation and test changes

--- tests/business/m_account/validate_account_id_length.sql
+++ tests/business/m_account/validate_account_id_length.sql
@@ -1,4 +1,4 @@
 ---Validates if the length of the account_id is 18 otherwise the test fails.
 SELECT *
-FROM "PRD_POC_DB"."BUSINESS_SCHEMA"."BUW_CUSTOMER_M_ACCOUNT"
-WHERE LEN(ACCOUNT_ID) != 18
+FROM "PRD_POC_DB"."RAW_SCHEMA"."ACCOUNT_RAW"
+WHERE LEN(ID) != 18

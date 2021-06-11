5ba6550490f4c822a79af0b629d283e9c689878a
sgarci34@its.jnj.com <sgarci34@its.jnj.com>
2021-06-11 08:23:50 +0000

    clone macro (working using command)

--- macros/clone_database.sql
+++ macros/clone_database.sql
@@ -1,6 +1,8 @@
 {% macro clone_database(database_name) -%}
-    
-    USE ROLE ACCOUNTADMIN;
-    CREATE OR REPLACE DATABASE test CLONE {{database_name}};    
-
+    {% set sql %}
+        USE ROLE ACCOUNTADMIN;
+        CREATE OR REPLACE DATABASE test CLONE {{database_name}};
+    {% endset %}
+        
+    {% do run_query(sql) %}
 {%- endmacro %}
